97a45dde4f1873cabfa4fd7c2bd7bbc576d35a68
tenpadrummer <4460drums@gmail.com>
2020-06-11 13:48:29 +0900

    set up rspec

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-commands-rspec'
   gem 'faker', require: false # for sample data in development
 end
 
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module Projects
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+
+      # RSpec 用のスペックファイルも一緒に作ってもらうよう Rails を設定
+      g.test_framework :rspec,
+        fixtures: false, # テストデータベースにレコードを作成するファイルの作成をスキップ
+        view_specs: false, # ビュースペックを作成しない
+        helper_specs: false, # ヘルパーファイル用のスペックを作成しない
+        routing_specs: false # routes.rb 用のスペックファイルの作成を省略
+      end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
