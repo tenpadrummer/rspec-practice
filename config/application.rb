4229340116cd676c3f2257184099b56297bca707
tenpadrummer <4460drums@gmail.com>
2020-06-11 16:41:07 +0900

    install factorybot

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,10 @@ module Projects
 
       # RSpec 用のスペックファイルも一緒に作ってもらうよう Rails を設定
       g.test_framework :rspec,
-        fixtures: false, # テストデータベースにレコードを作成するファイルの作成をスキップ
         view_specs: false, # ビュースペックを作成しない
         helper_specs: false, # ヘルパーファイル用のスペックを作成しない
-        routing_specs: false # routes.rb 用のスペックファイルの作成を省略
+        routing_specs: false, # routes.rb 用のスペックファイルの作成を省略
+        request_specs: false
       end
 
     # Settings in config/environments/* take precedence over those specified here.
