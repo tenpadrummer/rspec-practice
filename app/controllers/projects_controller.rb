c0a879ffaa319e049e130003bfba51fc2ab2a015
tenpadrummer <4460drums@gmail.com>
2020-06-24 17:39:33 +0900

    tdd controllers/projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -63,10 +63,13 @@ class ProjectsController < ApplicationController
   end
 
   def complete
-    @project.update_attributes!(completed: true)
-    redirect_to @project,notice: "Congratulations, this project is complete!"
+    if @project.update_attributes(completed: true)
+      redirect_to @project,
+        notice: "Congratulations, this project is complete!"
+    else
+      redirect_to @project, alert: "Unable to complete project."
+    end
   end
-
 
   private
     # Use callbacks to share common setup or constraints between actions.
